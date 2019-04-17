c3fee25736634b341a29f67ab9561835e0ce5ca4
andreabenf <andreabenf@gmail.com>
2019-04-17 13:20:47 -0300

    #136 - Adicionando estrutura para pegar a mensagem e enviar algo de volta

--- services/telegram/project/__init__.py
+++ services/telegram/project/__init__.py
@@ -6,19 +6,27 @@ bot_name = 'Aix'
 bot_username = 'Aix_chatbodebot'
 aix = telepot.Bot(telegram_token)
 
+app = Flask(__name__)
+app.config.from_object('project.config.DevelopmentConfig')
+
 def write_json(data, filename = 'Resposta.json'):
     with open(filename , 'w') as f:
         json.dump(data,f,indent=4, ensure_ascii=False)
 
 
 
-app = Flask(__name__)
-app.config.from_object('project.config.DevelopmentConfig')
-write_json(aix.getMe())
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
+    if request.method == 'POST':
+        msg = request.get_json()
+        chat_id= msg["message"]['chat']['id']
+        mensagem = msg['message']['text']
+        aix.sendMessage(chat_id,"olha, vou te imitar!\n" + mensagem)
+        write_json(msg, 'Resposta_request.json')
+        
+    
     return '<h1> lhul </h1>'
 
 
