302e2d35f3531551788726d45c595e96d17200e9
Pedro Igor <pedroigor.silva@outlook.com>
2019-04-17 18:10:35 -0300

    #137 - Request e parse da api do StackOverflow.
    Co-authored-by: Iuri Severo <nobreiuri@gmail.com>
    Co-authored-by: Gabriela Lemos <gabriela.leemoss@gmail.com>

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -2,6 +2,7 @@ from rasa_core_sdk import Action
 from rasa_core_sdk.events import SlotSet
 import requests
 import random
+import json
 
 class ActionOTRS(Action):
     def name(self):
@@ -25,7 +26,24 @@ class ActionPesquisaStackoverflow(Action):
 
     def run(self, dispatcher, tracker, domain):
         dispatcher.utter_message("Buscando...")
-        pesquisa = tracker.get_slot('pesquisa')
+        slot = tracker.get_slot('pesquisa')
+        dispatcher.utter_message(slot)
+        pesquisa = tracker.current_slot_values()['pesquisa']
         dispatcher.utter_message(pesquisa)
-        x = tracker.current_slot_values()['pesquisa']
-        dispatcher.utter_message(x)
+        if str(type(pesquisa))!="<class 'NoneType'>":
+            link = 'https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle='
+            site = '&site=stackoverflow'
+            resultado = requests.get(link+str(pesquisa)+site)
+            dicionario = json.loads(resultado.text)
+            links = []
+            for item in dicionario['items']:
+                if str((item)['is_answered']) == 'True':
+                    links.append(item['link'])
+                    
+                if len(links) == 5:
+                    break
+            if links:
+                for link in links:
+                    dispatcher.utter_message(link)
+            else:
+                dispatcher.utter_message("Bééé, infelizmente não encontrei nada sobre isso em minhas pesquisas. Poderia me perguntar com outras palavras?")
