54f112dcb946364f5646bd6f3141a3630604daf0
andreabenf <andreabenf@gmail.com>
2019-04-17 10:47:40 -0300

    -#136 - Criando função para exibir respostas json em arquivo

--- services/telegram/project/__init__.py
+++ services/telegram/project/__init__.py
@@ -1,18 +1,25 @@
 from flask import Flask, jsonify, request, Response
-import telepot
+import telepot,json
+from project.tokensz import telegram_token
 
 bot_name = 'Aix'
 bot_username = 'Aix_chatbodebot'
-token_telegramm = ''
+aix = telepot.Bot(telegram_token)
+
+def write_json(data, filename = 'Resposta.json'):
+    with open(filename , 'w') as f:
+        json.dump(data,f,indent=4, ensure_ascii=False)
+
+
 
 app = Flask(__name__)
 app.config.from_object('project.config.DevelopmentConfig')
-
+write_json(aix.getMe())
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
-    return '<h1>Podecre</h1>'
+    return '<h1> lhul </h1>'
 
 
 @app.route('/ping', methods=['GET'])
