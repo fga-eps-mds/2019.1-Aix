4cb326a3dc0df3bebe9a8189a85a2ac521ce89fd
pablodiegoss <pablodiegoss@hotmail.com>
2019-04-17 10:48:43 -0300

    Revert "[HOTFIX] - Deletando as pastas do bot."
    This reverts commit 4dce3abe7bd2bc8519a0f158d6dabed0193865cc.

---
+++ services/api/project/config.py
@@ -0,0 +1,18 @@
+class BaseConfig:
+    """Base configuration"""
+    TESTING = False
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration"""
+    pass
+
+
+class TestingConfig(BaseConfig):
+    """Testing configuration"""
+    TESTING = True
+
+
+class ProductionConfig(BaseConfig):
+    """Production configuration"""
+    pass
