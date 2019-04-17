4a04ee8457c6cbee7f8505a82032a0c74010ce5b
andreabenf <andreabenf@gmail.com>
2019-04-17 09:31:42 -0300

    #136 - começo da aplicação flask

--- services/telegram/project/__init__.py
+++ services/telegram/project/__init__.py
@@ -1,14 +1,24 @@
 from flask import Flask, jsonify, request, Response
+import telepot
 
-# instancia aplicativo flask
+bot_name = 'Aix'
+bot_username = 'Aix_chatbodebot'
+token_telegramm = ''
 
 app = Flask(__name__)
-
 app.config.from_object('project.config.DevelopmentConfig')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    return '<h1>Podecre</h1>'
 
 
 @app.route('/ping', methods=['GET'])
 def ping():
+
+
     return jsonify({
         'status': 'ok',
     })
