ec2c33fbc7a86b08a0a133fa85d37c12b279f1ce
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2019-05-02 10:47:55 -0300

    #234 - Configurando coverall e coverage e modificando o setup.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(name='Aix ChatBod',
       version='1.0',
       description='A simple Chatbot to teach Python for beginners.',
-      long_description='This project is from a college subject.A simple Django project about doctor''s prescription.',
       author='Guilherme Marques',
       packages=find_packages(),
+      test_suite="tests",
 )
