12e4c0ca9bf5755b58c6804612d5780f89f36e66
Yoshida-Eduardo <edry97@hotmail.com>
2019-05-11 11:18:44 -0300

    #232 separa build do travis em jobs

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,4 @@
 echo "Loging in to Dockerhub..."
 docker login --username=$DOCKERHUB_ID --password=$DOCKERHUB_PASSWORD
 echo "Pushing images..."
-docker push "$DOCKERHUB_ID"/aix-bot:latest
-docker push "$DOCKERHUB_ID"/aix-actions:latest
+docker push "$DOCKERHUB_ID"/"$SERVICE_NAME":latest
