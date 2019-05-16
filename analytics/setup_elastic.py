e1ee508008574d562bf711aed1a3a9c75d67f32c
André Freitas <andreabenf@gmail.com>
2019-05-16 11:06:10 -0300

    Update setup_elastic.py

--- analytics/setup_elastic.py
+++ analytics/setup_elastic.py
@@ -5,7 +5,9 @@ import argparse
 from elasticsearch import Elasticsearch
 
 parser = argparse.ArgumentParser(description='configures elastic')
-parser.add_argument('--task', '-t', default='setup', choices=['setup', 'delete'],)
+parser.add_argument('--task', '-t', 
+                    default='setup',
+                    choices=['setup', 'delete'],)
 args = parser.parse_args()
 
 logging.basicConfig(level=logging.DEBUG)
