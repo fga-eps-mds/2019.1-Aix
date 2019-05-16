0c41477f38be94974dfb274c57526eedbe710665
Iuri Severo <nobreiuri@gmail.com>
2019-05-16 09:02:28 -0300

    #283 - Testando função should fallback
    Co-authored-by: Gabriela Lemos <gabriela.leemoss@gmail.com>

--- tests/test_fallback.py
+++ tests/test_fallback.py
@@ -20,7 +20,34 @@ def test_standard_featurizer(custom_fallback):
     response = custom_fallback._standard_featurizer()
     assert response == None
 
-
-def test_should_fallback(custom_fallback):
+def test_should_fallback1(custom_fallback):
+	# high confidence
     response = custom_fallback.should_fallback(1, "2")
     assert response == False
+
+def test_should_fallback2(custom_fallback):
+	# low confidence
+    response = custom_fallback.should_fallback(0.3, "2")
+    assert response == True
+
+def test_should_fallback3(custom_fallback):
+	# high confidence + last action fallback
+    response = custom_fallback.should_fallback(1.0, "action_default_fallback")
+    assert response == False
+
+def test_should_fallback4(custom_fallback):
+	# low confidence + last action fallback
+    response = custom_fallback.should_fallback(0.5, "action_default_fallback")
+    assert response == False
+
+def test_should_fallback5(custom_fallback):
+	# high confidence + last action None
+    response = custom_fallback.should_fallback(1.0, None)
+    assert response == False
+
+def test_should_fallback6(custom_fallback):
+	# low confidence + last action None
+    response = custom_fallback.should_fallback(0.4, None)
+    assert response == False
+
+
