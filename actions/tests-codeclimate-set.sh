99700bee3f6de84080b5e8b8d4495e1d1ad60bfd
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2019-05-28 19:52:49 -0300

    #321 - Adicionando script pra actions e redirando scripts redundantes.
    Co-authored-by: Eduardo Yoshida <edry97@hotmail.com>

--- actions/tests-codeclimate-set.sh
+++
@@ -1,4 +0,0 @@
-cd actions
-curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
-chmod +x ./cc-test-reporter
-./cc-test-reporter before-build
