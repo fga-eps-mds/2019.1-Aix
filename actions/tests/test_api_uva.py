07595dbece179ec5d4c83fbe23cd5e1169a07a7f
andreabenf <andreabenf@gmail.com>
2019-06-07 09:06:01 -0300

    #379 - Adicinando template de testes para cada função da api_uva

--- actions/tests/test_api_uva.py
+++ actions/tests/test_api_uva.py
@@ -2,3 +2,37 @@ import pytest
 import os
 import sys
 import inspect
+import api_uva
+
+
+
+
+def test_get_params():
+    api_uva.get_params()
+
+def test_get_soup():
+    api_uva.get_soup()
+
+def test make_login():
+    api_uva.make_login()
+
+def test_get_code(path):
+    api_uva.get_code()
+
+def test_get_problem():
+    api_uva.get_problem()
+
+def test_get_problem_by_id():
+    api_uva.get_problem_by_id()
+
+def test_get_problem_by_number():
+    api_uva.get_problem_by_number()
+
+def test_submeter_um_problema():
+    api_uva.submeter_um_problema()
+
+def test_username_para_userid():
+    api_uva.username_para_userid()
+
+def test_resultado_ultima_submissao():
+    api_uva.resultado_ultima_submissao()
