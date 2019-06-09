5f397716e64e70a0b3889014d1c5af2261157286
Gustavo Veloso <gustablealves@gmail.com>
2019-06-09 16:22:48 -0300

    #371 - Criação de script que concatena os arquivos modelos com o conteúdo
    Co-authored-by: Gabriela Lemos <gabriela.leemoss@gmail.com>

--- scripts/create_intents.py
+++ scripts/create_intents.py
@@ -1,2 +1,30 @@
-def createIntents(conteudo, tipo_de_intent):
-    
+tipo_de_intent = raw_input("Insira o tipo de intent:")
+
+print("Insira o nome do conteudo e suas variacoes")
+print("Quando acabar, digite QQ para continuar a execucao")
+
+conteudos = []
+content = ""
+
+while(content != "QQ"):
+    content = raw_input()
+    if content == "QQ":
+        break
+    conteudos.append(content)
+
+
+filename = "intents/" + tipo_de_intent + ".txt"
+fileToWrite = open("result.txt", "w")
+
+fileToWriteContent = []
+
+for conteudo in conteudos:
+    fileToRead = open(filename, "r")
+    for line in fileToRead:
+        fileToWriteContent.append(line.replace("[CONTEUDO]", conteudo))
+    fileToRead.close()
+
+fileToWriteContent.sort()
+fileToWrite.writelines(fileToWriteContent)
+
+fileToWrite.close()    
