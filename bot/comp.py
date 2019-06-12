82d440630005ce867f71cae48e563fb81d4ae342
Pedro Igor <pedroigor@aluno.unb.br>
2019-06-11 22:22:46 -0300

    #408 - Adicionando retornos da função. 0 caso sucesso, 1 caso falha mais a descrição da falha.

--- bot/comp.py
+++ bot/comp.py
@@ -1,5 +1,11 @@
 import yaml
-with open('domain.yml', 'r') as domain:
-    domain_data = yaml.safe_load(domain)
-for i in domain_data:
-    print(domain_data[i])
+import sys
+domain_data = {}
+try:
+    with open('domain.yml', 'r') as domain:
+        domain_data = yaml.safe_load(domain)
+    print(domain_data)
+except:
+    err_type, error, traceback = sys.exc_info()
+    print(err_type, error, traceback)
+    raise sys.exit(1)
